bb3093622af1d8a6eb6481b3a7365ecb3d714f0d
rk <ranjitkale0001@gmail.com>
2024-11-21 23:34:28 +0530

    intial spring boot

---
+++ Good_Care_Hospital_Management_System_SpringBoot/src/main/java/org/Good_Care_HospitalMS/GoodCareHospitalManagementSystemSpringBootApplication.java
@@ -0,0 +1,14 @@
+package org.Good_Care_HospitalMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodCareHospitalManagementSystemSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodCareHospitalManagementSystemSpringBootApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ Good_Care_Hospital_Management_System_SpringBoot/src/test/java/org/Good_Care_HospitalMS/GoodCareHospitalManagementSystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Good_Care_HospitalMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodCareHospitalManagementSystemSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
